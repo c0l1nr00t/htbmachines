6fb6fb805c77a74f7313e256e3ba960542e88de2
c0l1nr00t <mr.c0l1nr00t@gmail.com>
2023-04-12 23:06:43 -0600

    Add url to download machines of Google Drive

--- htbmachines.sh
+++ htbmachines.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Variables globales.
-url_htb="https://docs.google.com/spreadsheets/u/0/d/1dzvaGlT_0xnT-PGO27Z_4prHgA8PHIpErmoWdlUrSoA/gviz/tq?tqx=out:html";
-url_vulnhub="https://docs.google.com/spreadsheets/u/0/d/1dzvaGlT_0xnT-PGO27Z_4prHgA8PHIpErmoWdlUrSoA/gviz/tq?tqx=out:html";
+url_vulnhub="https://docs.google.com/spreadsheets/d/1dzvaGlT_0xnT-PGO27Z_4prHgA8PHIpErmoWdlUrSoA/export?format=csv&id=1dzvaGlT_0xnT-PGO27Z_4prHgA8PHIpErmoWdlUrSoA&gid=810933541"
+url_htb="https://docs.google.com/spreadsheets/d/1dzvaGlT_0xnT-PGO27Z_4prHgA8PHIpErmoWdlUrSoA/export?format=csv&id=1dzvaGlT_0xnT-PGO27Z_4prHgA8PHIpErmoWdlUrSoA&gid=0";
 declare -i counter=0;
 
 #Colours
@@ -50,39 +50,69 @@ function hola_cursor()
 
 function ctrl_c()
 {
-  echo -e "\n ${yellowColour}[!]${endColour} ${redColour}Saliendo ... ${endColour} \n";
-  hola_cursor;  
-  exit 1;
+  echo -e "\n ${yellowColour}[!]${endColour} ${redColour}Saliendo... ${endColour} \n";
+  exit_tool 1;
 }
+
+function exit_tool()
+{
+  code=$1;
+  hola_cursor;
+  exit $code;
+}
+
 
 # Funcionalidades del sistema
-
-#Panel de ayuda
-function helpPanel()
+# catch error default case
+function catch_default()
 {
-  echo -e "\n ${yellowColour}[?]${endColour}${grayColour}Panel de ayuda${endColour}${grayColour}:${endColour}\n";
-  echo -e "\n\t${yellowColour}-h: ${endColour}${grayColour}Muestra este panel de ayuda.${endColour}\n";
+  echo -e "\n ${redColour}Falta de parametros para la busqueda ${endColour} \n";
+  helpPanel;
+  exit_tool 1;
 }
 
+# Panel de ayuda
+function helpPanel()
+{
+  echo -e "\n ${yellowColour}[?]${endColour}${grayColour} PANEL DE AYUDA${endColour}${grayColour}:${endColour}\n";
+  echo -e "\n\t${yellowColour}-h: ${endColour}${grayColour}Muestra este panel de ayuda.${endColour}\n";
+  echo -e "\n\t${yellowColour}-u: ${endColour}${grayColour}Update máquinas.${endColour}\n";
+  echo -e "\n\t${yellowColour}-m: ${endColour}${grayColour}Buscar por nombre de la maquina.${endColour}\n";
+}
+
+# Funcion para hacer el download de los archivos y verificar cambios
+function updateFiles()
+{
+  #revisando si existe las maquinas de HTB
+  if [ ! -f machines_htb ]; then
+    echo -e "\n ${yellowColour}[+]${endColour}${grayColour}Descargando máquinas de HTB...${endColour}";
+    wget ${url_htb} -q -O machines_htb; 
+    wget ${url_vulnhub} -q -O machines_vulnhub;
+  else
+    echo -e "\n ${yellowColour}[+] Comprobando actualizaciones de HTB ...${endColour}";
+  fi
+}
 
 # Logica del programa
 adios_cursor;
 banner;
 
 # Options
-while getopts "m:h" arg; do
+while getopts "m:hu" arg; do
   case $arg in 
-    m)nameMachine=$OPTARG;let counter+=1;;
+    m)nameMachine=$OPTARG;let counter=1; ;;
+    u)let counter=2; ;;
+    h)let counter=3; ;;
   esac done;
 
 #Valuacion de opciones
 case $counter in
   1) searchByName;;
-  0) helpPanel;;
-  *) echo -e"\n${redColour}No es una opcion correcta${endColour}\n"; helpPanel;;
+  2) updateFiles;;
+  3) helpPanel;;
+  *) catch_default;;
 esac;
 
 
 # Terminando ejecucion.
-hola_cursor;
-exit 0;
+exit_tool 1;
